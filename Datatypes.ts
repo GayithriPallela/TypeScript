5a046d0689385adc9e463307bf789d5059262874
GayithriPallela <gayithripallela2006@gmail.com>
2026-07-20 17:18:54 +0530

    Added Datatypes.ts

---
+++ Datatypes.ts
@@ -0,0 +1,25 @@
+// strings 
+let a:string="hi";
+let b:string="hello";
+let c:string="good";
+console.log(a);
+console.log(b);
+console.log(c);
+//number
+let x:number=10;
+let y:number=1.35;
+let z:number=9;
+console.log(x +" "+  y +" " + z);
+//bool
+let p:boolean=true;
+let q:boolean=false;
+console.log(p +" " +  q);
+//any 
+let e:any="hi";
+console.log(e);
+e=15;
+console.log(e);
+//undefined 
+let demo:undefined|string="hello";
+console.log(demo);
+
